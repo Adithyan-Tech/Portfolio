203a90022a22c4508b19f8a92a455295129b334d
tAdithyan <adithyanttech@gmail.com>
2023-10-02 10:59:42 +0530

    structure of navbar:

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '/home/vishnu/Desktop/portfolio/porttfolio/src/assets/assets/logo.png'
+const Navbar = () => {
+  return (
+ <nav className='navbar' >
+  <img src={logo} alt="logo"  className='logo'/>
+  <div className="desktopMenu">
+
+  </div>
+<button className="desktopMenuBtn">
+  <img src="" alt="" className="desktopMenuImg" />
+   Contact Me
+</button>
+
+ </nav>
+
+  )
+}
+
+export default Navbar
