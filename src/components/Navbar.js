22127d6c40db47803ff315a9fc38d6cf1a769f0a
tAdithyan <adithyanttech@gmail.com>
2023-10-02 11:30:00 +0530

    logo created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '/home/vishnu/Desktop/portfolio/porttfolio/src/assets/assets/logo.png'
+import {Link} from 'react-scroll'
 const Navbar = () => {
   return (
  <nav className='navbar' >
   <img src={logo} alt="logo"  className='logo'/>
   <div className="desktopMenu">
+ <Link className="desktopMenuListIteams">Home</Link>
+ <Link className="desktopMenuListIteams">Clint</Link>
+ <Link className="desktopMenuListIteams">About</Link>
+ <Link className="desktopMenuListIteams">portfolio</Link>
 
   </div>
 <button className="desktopMenuBtn">
