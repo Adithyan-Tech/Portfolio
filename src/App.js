f887734dabf84cc3d8dbcd59b4f32f06e1a04543
tAdithyan <adithyanttech@gmail.com>
2023-10-23 12:48:04 +0530

    create base of clint section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+
 import Navbar from './components/Navbar';
 import Intro from './components/intro';
 import About from './components/About';
 import Works from './components/works'
+import Clint from './components/clint';
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +14,8 @@ function App() {
      <Intro />
      <About />
      <Works />
+     <Clint />
+
     </div>
   );
 }
---
+++ src/components/clint.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./clint.css";
+import walmart from '../assets/assets/walmart.png';
+import Adobe from '../assets/assets/adobe.png';
+import microsoft from '../assets/assets/microsoft.png';
+import facebook from '../assets/assets/facebook.png';
+const Clint = () => {
+  return (
+    <div>
+      <section className="contactpage">
+        <div id="clints">
+          <h1 className="contactPageTitle">MY CLINTS</h1>
+          <p className="clintDesc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
+            quaerat perferendis ex porro enim doloremque laborum accusantium
+            soluta alias nostrum! Veniam doloremque ea molestiae laborum quia
+            consectetur vitae perferendis est.
+          </p>
+          <div className="clintimgs">
+            <img src={walmart} alt="walmart" className="clintImg" />
+            <img src={Adobe} alt="Adobe" className="clintImg" />
+            <img src={microsoft} alt="microsoft" className="clintImg" />
+            <img src={facebook} alt="facebook" className="clintImg" />
+          </div>
+        </div>
+        <div id="contact"></div>
+      </section>
+    </div>
+  );
+};
+
+export default Clint;
