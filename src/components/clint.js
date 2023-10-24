efc378cd72256b9ae634f842ab7be01f55317561
tAdithyan <adithyanttech@gmail.com>
2023-10-24 09:10:37 +0530

    stucture of contactsection

--- src/components/clint.js
+++ src/components/clint.js
@@ -4,6 +4,10 @@ import walmart from '../assets/assets/walmart.png';
 import Adobe from '../assets/assets/adobe.png';
 import microsoft from '../assets/assets/microsoft.png';
 import facebook from '../assets/assets/facebook.png';
+import facbookicon from '../assets/assets/facebook-icon.png';
+import twittericon from '../assets/assets/twitter.png';
+import instagramicon from '../assets/assets/instagram.png';
+
 const Clint = () => {
   return (
     <div>
@@ -23,7 +27,22 @@ const Clint = () => {
             <img src={facebook} alt="facebook" className="clintImg" />
           </div>
         </div>
-        <div id="contact"></div>
+        <div id="contact">
+          <h1 className="contactPageTitle">Conatct Me</h1>
+          <span className="contactDesc"> please fill the fom below to discuss any work oppertunities.</span>
+          <form className="contactForm">
+            <input type="text" className="name" placeholder="enter name"/>
+            <input type="text" className="email" placeholder="enter email" />
+            <textarea  className='msg'name="message" rows='5'placeholder="your message"></textarea>
+            <button className="submit" type="submit" value='send'></button>
+            <div className="links">
+             <img src={facbookicon} alt='facbookicon' className="link"></img>
+             <img src={twittericon} alt='twittericon' className="link"></img>
+             <img src={instagramicon} alt='instagramicon' className="link"></img>
+            </div>
+          </form>
+        </div>
+
       </section>
     </div>
   );
